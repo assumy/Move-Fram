566e92449713c9338fb2f2b59fefb37a609d028f
assumy <2315834730@qq.com>
2020-08-10 12:49:03 +0800

    升级版；兼容链式结构

--- Move.js
+++ Move.js
@@ -1,10 +1,18 @@
-function move(obj,name,target){
+function move(obj,name,target,fn){
 				clearInterval(obj.timeA)
 				obj.timeA=setInterval(function(){
 					let nowStyle=parseInt(getComputedStyle(obj)[name])
-					let speed=(target-obj.opa)/10
+					let speed=(target-nowStyle)/10
+					// 速度取整,三元函数
+					speed=(speed>0)? Math.ceil(speed):Math.floor(speed)
+					
+					
+					
+					
 					if (nowStyle==target) {
 						clearInterval(obj.timeA)
+						// 链式结构
+						if(fn){fn()}
 					} else{
 						obj.style[name]=nowStyle+speed+'px'
 					}
